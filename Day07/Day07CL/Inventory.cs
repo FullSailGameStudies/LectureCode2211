285c567ae3a641959058a6eed3fb6cfd68232457
Garrett Girod <girodg@hotmail.com>
2022-11-07 10:28:05 -0500

    properties

--- Day07/Day07CL/Inventory.cs
+++ Day07/Day07CL/Inventory.cs
@@ -11,5 +11,24 @@ namespace Day07CL
     {
         private int _capacity = 0;
         private List<string> _items = new List<string>();
+
+        public int Capacity
+        {
+            get { return _capacity; }
+            set
+            {
+                if(value > 0)
+                    _capacity = value;
+            }
+        }
+        public int Count
+        {
+            get { return _items.Count; }
+        }
+        public List<string> Items
+        {
+            get { return _items; }
+            private set { _items = value; }
+        }
     }
 }
